c311cf89b4b7898007fa64a68eb05e23511963c4
Huw <big-huw@hotmail.co.uk>
2022-01-08 17:51:42 +0000

    Created a clear display function to delete any values in the display text input

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function display(value) {
     document.getElementById("display").value += value;
    }
 
-
-   
+   function clearScreen() {
+    document.getElementById("display").value = "";
+   }
